c08718aee238de066e71f06737166e26bfccad89
Vishnu <vappu5429@gmail.com>
2023-05-06 21:59:40 +0530

    Added text animation

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,12 +5,12 @@ type Props = {};
 
 function Hero({}: Props) {
   return (
-    <div className="max-w-7xl grid md:grid-cols-2 h-[90vh] py-8 mx-auto  ">
+    <div className="max-w-7xl  grid md:grid-cols-2 h-[90vh] py-8 mx-auto  ">
       <div className=" flex flex-col justify-center gap-4 ">
-        <h1 className="p-2 text-white font-bold text-6xl animate-bounce">
+        <h1 className="p-2 text-white font-bold text-3xl sm:text-6xl animate-bounce">
           Shamith Kumar Jain
         </h1>
-        <h3 className="text-xl text-white font-semibold">
+        <h3 className="sm:text-xl text-white font-semibold">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
           placeat dicta ipsa voluptas?.....
         </h3>
@@ -25,7 +25,7 @@ function Hero({}: Props) {
           alt="/vercel.svg"
           height={100}
           width={100}
-          className="w-full h-[90%]"
+          className="w-full h-[80%] sm:h-[90%]"
         />
       </div>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,14 @@ import Navbar from "../../components/Navbar";
 export default async function Home() {
   // bg - [rgb(36, 36, 36)];
   return (
-    <div className="bg-gray-900 mx-auto min-h-screen p-4">
+    <div className="bg-gray-900   snap-y snap-mandatory mx-auto min-h-screen p-4">
       <Navbar />
-      <Hero />
-      <About />
+      <section className="snap-start">
+        <Hero />
+      </section>
+      <section className="snap-center">
+        <About />
+      </section>
     </div>
   );
 }
