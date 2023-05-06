8744ac37abb9791de116eb4d76b07bb57e0fbd07
Vishnu <vappu5429@gmail.com>
2023-05-06 20:46:56 +0530

    added transition to image

--- components/About.tsx
+++ components/About.tsx
@@ -1,13 +1,18 @@
+"use client";
 import React from "react";
 import Image from "next/image";
 import { MyImage } from "../assets";
-
+import { motion } from "framer-motion";
 type Props = {};
 
 function About({}: Props) {
   return (
     <div className="max-w-7xl mx-auto h-screen px-4 gap-8 flex flex-col md:flex-row  justify-center items-center">
-      <div>
+      <motion.div
+        initial={{ x: "-100vw", opacity: 0.5 }}
+        animate={{ opacity: 1, x: 0, type: "spring" }}
+        transition={{ duration: 1 }}
+      >
         <Image
           alt="vercel"
           height={40}
@@ -16,8 +21,8 @@ function About({}: Props) {
           className="object-cover rounded-lg  h-[30vh] md:h-[50vh] w-[30vh] md:w-[30vw]"
           src={MyImage}
         />
-      </div>
-      <div className="flex gap-4 flex-col h-[30vh] md:h-[50vh] w-[30vh] md:w-[50vw] leading-5 items-center justify-center">
+      </motion.div>
+      <div className="flex gap-4 text-white flex-col h-[30vh] md:h-[50vh] w-[30vh] md:w-[50vw] leading-5 items-center justify-center">
         <h1 className="font-bold text-xl text-center">
           Here's a little information about me.
         </h1>
