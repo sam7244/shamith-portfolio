f718f80bbd31a826801851a0323275e14183b558
Vishnu <vappu5429@gmail.com>
2023-05-16 19:15:37 +0530

    Maybe cleared the skill animation delay

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -6,15 +6,6 @@ import Skill from "./Skill";
 const Skills = ({ skills }: any) => {
   return (
     <motion.div
-      initial={{
-        opacity: 0,
-      }}
-      whileInView={{
-        opacity: 1,
-      }}
-      transition={{
-        duration: 1.5,
-      }}
       className=" flex relative flex-col text-center md:text-left xl:flex-row
     max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center"
     >
@@ -28,11 +19,21 @@ const Skills = ({ skills }: any) => {
         Hover over a skill for current proficiency{" "}
       </h3>
       <div className="grid grid-cols-4  gap-5 ">
-        {skills?.slice(0, skills?.length / 2).map((skill: any) => (
-          <Skill key={skill?._id} skill={skill} directionLeft={true} />
+        {skills?.slice(0, skills?.length / 2).map((skill: any, idx: number) => (
+          <Skill
+            key={skill?._id}
+            idx={idx}
+            skill={skill}
+            directionLeft={true}
+          />
         ))}
-        {skills?.slice(skills?.length / 2, skills.length).map((skill: any) => (
-          <Skill key={skill?._id} skill={skill} directionLeft={false} />
+        {skills?.slice(skills?.length / 2).map((skill: any, idx: number) => (
+          <Skill
+            key={skill?._id}
+            idx={idx}
+            skill={skill}
+            directionLeft={false}
+          />
         ))}
       </div>
     </motion.div>
